91db11aeceeed04761f93b6d8d69c212c9300db2
Richard Knoll <riknoll@microsoft.com>
2026-08-24 15:55:31 -0700

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# arcade-piano-roll
+
+A Microsoft MakeCode extension for Arcade that allows you to create songs using a piano roll editor.
+
+
+## How to use
+
+Open a project in the [Microsoft MakeCode editor for Arcade](https://arcade.makecode.com/) and click on the **Extensions** button in the **Advanced** section of the toolbox. Search for `piano-roll` and click on the extension to add it to your project.
---
+++ api.ts
@@ -0,0 +1,23 @@
+//% color="#E63022"
+//% block="Piano Roll"
+//% icon="\uf001"
+namespace pianoRoll {
+    //% blockId=arcade_piano_roll_create_song
+    //% block="piano roll $song"
+    //% song.fieldEditor=pianoroll
+    //% song.fieldOptions.decompileLiterals=true
+    //% song.fieldOptions.taggedTemplate="hex;assets.song"
+    //% song.fieldOptions.decompileIndirectFixedInstances="true"
+    //% song.fieldOptions.decompileArgumentAsString="true"
+    //% song.fieldOptions.showTimeSignature="true"
+    //% song.fieldOptions.showSnapControls="true"
+    //% song.snippet="hex`00780004080100`"
+    //% song.pySnippet='hex("""00780004080100""")'
+    //% toolboxParent=music_playable_play
+    //% toolboxParentArgument=toPlay
+    //% duplicateShadowOnDrag
+    //% help=music/create-song
+    export function createSong(song: Buffer): music.Playable {
+        return new music.sequencer.Song(song);
+    }
+}
